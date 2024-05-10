c50ea2d1746b4040f08d9cb1b529e8adcbe778c3
Yauchin Lam <yauchin.lam@outlook.com>
2024-05-09 23:21:05 -0400

    CDK lambda with test lambda

---
+++ stack/cdk/cdk_stack.py
@@ -0,0 +1,27 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_lambda as _lambda,
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class CdkStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        self.prediction_lambda = _lambda.DockerImageFunction(
+            scope=self,
+            id="ExampleDockerLambda",
+            # Function name on AWS
+            function_name="ExampleDockerLambda",
+            # Use aws_cdk.aws_lambda.DockerImageCode.from_image_asset to build
+            # a docker image on deployment
+            code=_lambda.DockerImageCode.from_image_asset(
+                # Directory relative to where you execute cdk deploy
+                # contains a Dockerfile with build instructions
+                directory="lambda/Dockerfile"
+            ),
+        )
+        
