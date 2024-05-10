dae24e04684494572eeb16c9cc398508e5d4eac6
Yauchin Lam <yauchin.lam@outlook.com>
2024-05-09 23:25:15 -0400

    Corrected cdk stack

--- stack/cdk/cdk_stack.py
+++ stack/cdk/cdk_stack.py
@@ -15,13 +15,13 @@ class CdkStack(Stack):
             scope=self,
             id="ExampleDockerLambda",
             # Function name on AWS
-            function_name="ExampleDockerLambda",
+            function_name="handler",
             # Use aws_cdk.aws_lambda.DockerImageCode.from_image_asset to build
             # a docker image on deployment
             code=_lambda.DockerImageCode.from_image_asset(
                 # Directory relative to where you execute cdk deploy
                 # contains a Dockerfile with build instructions
-                directory="lambda/Dockerfile"
+                directory="lambda"
             ),
         )
         
